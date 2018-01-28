6d0446b3ac38b5dd6ae559da579e2cd8afa341a3
Samuel Boulanger <samuelboulanger1@gmail.com>
2018-01-27 19:18:20 -0800

    init of SB_MNIST

---
+++ Projects/SB_MNIST/cnn_mnist.py
@@ -0,0 +1,13 @@
+#Samuel Boulanger 1.27.18
+#Beginning MNIST tutorial for Tensorflow
+
+import numpy as np
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+
+if __name__ == "__main__":
+    tf.app.run()
+
